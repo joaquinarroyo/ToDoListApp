79c9fc7f665d433eb8811bae0338822a12c55154
joaquin <joaquin.arroyo100@gmail.com>
2022-02-10 22:26:10 -0300

    Create routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,22 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {Home} from './views/Home';
-import {Tasks} from './components/Tasks';
+import {EditFolder} from './components/folder/EditFolder';
+import {EditTask} from './components/task/EditTask';
+import { Folders } from './components/folder/Folders';
+import {Tasks} from './components/task/Tasks';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/folders.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Folders />} />
+        <Route path="/editFolder" element={<EditFolder/>} />
+        <Route path="/viewTasks" element={<Tasks/>} />
+        <Route path="/editTask" element={<EditTask/>} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App;
