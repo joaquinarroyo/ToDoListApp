a41c83296ad5be091097b47b18e196bacc239454
joaquin <joaquin.arroyo100@gmail.com>
2022-02-11 18:42:52 -0300

    Update and finish shell script

--- scripts/toDoApp.sh
+++ scripts/toDoApp.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+PASSWORD=$1 # The first argument is the password
 JAR_NAME="back-0.0.1-SNAPSHOT.jar"
-OPTIONS=" -c /home/joaquin/Desktop/Ejercicio/ToDoListApp/back/src/main/resources/application.properties"
+OPTIONS="/home/joaquin/Desktop/Ejercicio/ToDoListApp/back/src/main/resources/application.properties"
+OPTIONS2="-c /home/joaquin/Desktop/Ejercicio/ToDoListApp/back/src/main/resources/application.properties"
+
+# Create the database if it not exists
+mysql -u root -p$PASSWORD -e "CREATE DATABASE IF NOT EXISTS todoApp"
+
+# Deletes de row that has the root password and inserts the new password
+sed -i.bak -e '8d' ${OPTIONS}
+echo "spring.datasource.password="${PASSWORD} >> ${OPTIONS}
+
+# Run the application
 cd /home/joaquin/Desktop/Ejercicio/ToDoListApp/back
-"${JAVA_HOME}/bin/java" ${JAVA_OPTIONS} -jar $JAR_NAME $OPTIONS & cd /home/joaquin/Desktop/Ejercicio/ToDoListApp/front && npm start
+"${JAVA_HOME}/bin/java" ${JAVA_OPTIONS} -jar ${JAR_NAME} ${OPTIONS2} & cd /home/joaquin/Desktop/Ejercicio/ToDoListApp/front && npm start
