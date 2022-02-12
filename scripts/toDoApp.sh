fc2280927eca107b1baa5db17a8b70a1cca0e2e9
joaquin <joaquin.arroyo100@gmail.com>
2022-02-12 18:42:08 -0300

    Last change on .sh file

--- scripts/toDoApp.sh
+++ scripts/toDoApp.sh
@@ -2,8 +2,8 @@
 
 PASSWORD=$1 # The first argument is the password
 JAR_NAME="back-0.0.1-SNAPSHOT.jar"
-OPTIONS="/home/joaquin/Desktop/Ejercicio/ToDoListApp/back/src/main/resources/application.properties"
-OPTIONS2="-c /home/joaquin/Desktop/Ejercicio/ToDoListApp/back/src/main/resources/application.properties"
+OPTIONS="../back/src/main/resources/application.properties"
+OPTIONS2="-c ../back/src/main/resources/application.properties"
 
 # Create the database if it not exists
 mysql -u root -p$PASSWORD -e "CREATE DATABASE IF NOT EXISTS todoApp"
@@ -13,5 +13,5 @@ sed -i.bak -e '8d' ${OPTIONS}
 echo "spring.datasource.password="${PASSWORD} >> ${OPTIONS}
 
 # Run the application
-cd /home/joaquin/Desktop/Ejercicio/ToDoListApp/front
-npm start & cd /home/joaquin/Desktop/Ejercicio/ToDoListApp/back && "${JAVA_HOME}/bin/java" ${JAVA_OPTIONS} -jar ${JAR_NAME} ${OPTIONS2}
+cd ../front
+npm start & cd ../back && "${JAVA_HOME}/bin/java" ${JAVA_OPTIONS} -jar ${JAR_NAME} ${OPTIONS2}
